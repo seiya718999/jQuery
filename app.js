88318c70c5e962adea6b369fef25ccfad78b552c
SeiyaKoga <138374827+seiya718999@users.noreply.github.com>
2023-12-22 05:58:24 +0000

    finish

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ $(function(){
   });
 });
 
+$(function(){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'height': '100px',
+      'width': '200px'
+    }).slideUp(1000);
+  });
+});
